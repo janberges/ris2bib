c674f0b95add80284e65deaa490e9b1b4681ff03
Jan Berges <jan.berges@uni-bremen.de>
2019-12-17 14:42:02 +0100

    Protect capitalization of chemical elements

--- format.py
+++ format.py
@@ -46,6 +46,19 @@ names = [
     'Yosida',
     ]
 
+elements = [
+    'H',  'He', 'Li', 'Be', 'B',  'C',  'N',  'O',  'F',  'Ne', 'Na', 'Mg',
+    'Al', 'Si', 'P',  'S',  'Cl', 'Ar', 'K',  'Ca', 'Sc', 'Ti', 'V',  'Cr',
+    'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr',
+    'Rb', 'Sr', 'Y',  'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd',
+    'In', 'Sn', 'Sb', 'Te', 'I',  'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd',
+    'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf',
+    'Ta', 'W',  'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po',
+    'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U',  'Np', 'Pu', 'Am', 'Cm',
+    'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs',
+    'Mt', 'Ds', 'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og',
+    ]
+
 def protected(token, previous=None):
     upper = re.search('[A-Z]', token)
 
@@ -66,6 +79,14 @@ def protected(token, previous=None):
 
         for name in names:
             if token.startswith(name):
+                return True
+
+        # e.g. "Li"
+
+        letters = ''.join(re.findall('[a-zA-Z]+', token))
+
+        for element in elements:
+            if letters == element:
                 return True
 
         # Start of further title:
